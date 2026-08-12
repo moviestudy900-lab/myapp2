43b7acbc9fcaa082ad0819a92793ba73821394d2
moviestudy900-lab <moviestudy900@gmail.com>
2026-08-12 15:56:35 +0600

    Update bridge.js

--- capacitor-src/bridge.js
+++ capacitor-src/bridge.js
@@ -3,5 +3,6 @@ import { LocalNotifications } from '@capacitor/local-notifications';
 import { NativeBiometric } from 'capacitor-native-biometric';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Share } from '@capacitor/share';
+import { Browser } from '@capacitor/browser';
 
-window.CapBridge = { Capacitor, LocalNotifications, NativeBiometric, Filesystem, Directory, Share };
+window.CapBridge = { Capacitor, LocalNotifications, NativeBiometric, Filesystem, Directory, Share, Browser };
