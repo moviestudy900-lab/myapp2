d96ef9f4ca5594c5459603a2a3089f73216e10a7
moviestudy900-lab <moviestudy900@gmail.com>
2026-08-12 07:56:53 +0600

    Delete capacitor-src/bridge.js

--- capacitor-src/bridge.js
+++
@@ -1,5 +0,0 @@
-import { Capacitor } from '@capacitor/core';
-import { LocalNotifications } from '@capacitor/local-notifications';
-import { NativeBiometric } from 'capacitor-native-biometric';
-
-window.CapBridge = { Capacitor, LocalNotifications, NativeBiometric };
