f2845d1cd2c8c249c1e7b8add7963bc3d1bf699b
moviestudy900-lab <moviestudy900@gmail.com>
2026-08-14 18:08:28 +0600

    Add files via upload

---
+++ capacitor-src/bridge.js
@@ -0,0 +1,9 @@
+import { Capacitor } from '@capacitor/core';
+import { LocalNotifications } from '@capacitor/local-notifications';
+import { NativeBiometric } from 'capacitor-native-biometric';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
+import { Browser } from '@capacitor/browser';
+import { App } from '@capacitor/app';
+
+window.CapBridge = { Capacitor, LocalNotifications, NativeBiometric, Filesystem, Directory, Share, Browser, App };
