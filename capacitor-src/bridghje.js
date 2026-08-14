570b02a891dcbba791290fd1e91940ef890f5241
moviestudy900-lab <moviestudy900@gmail.com>
2026-08-14 18:08:49 +0600

    Delete capacitor-src/bridghje.js

--- capacitor-src/bridghje.js
+++
@@ -1,8 +0,0 @@
-import { Capacitor } from '@capacitor/core';
-import { LocalNotifications } from '@capacitor/local-notifications';
-import { NativeBiometric } from 'capacitor-native-biometric';
-import { Filesystem, Directory } from '@capacitor/filesystem';
-import { Share } from '@capacitor/share';
-import { Browser } from '@capacitor/browser';
-
-window.CapBridge = { Capacitor, LocalNotifications, NativeBiometric, Filesystem, Directory, Share, Browser };
